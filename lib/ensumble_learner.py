f96fefd9dd3647bc26485f735dc79d541403f98f
j129008 <j129008@gmail.com>
2017-12-05 18:00:22 +0800

    thread gen model

--- lib/ensumble_learner.py
+++ lib/ensumble_learner.py
@@ -1,14 +1,29 @@
 from sklearn.model_selection import train_test_split
 from lib.learner import Learner
 from collections import Counter
+from queue import Queue
+from threading import Thread
+from tqdm import tqdm as bar
 
 class Bagging(Learner):
     def __init__(self, path):
         super().__init__(path)
         self.model_list = []
+        self.queue = Queue()
+    def gen_model(self, train_size, c1, c2):
+        self.queue.put( super().train(sub_train=train_size) )
     def train(self, voter=2, train_size=0.05, c1=0, c2=1):
-        for _ in range(voter):
-            self.model_list.append(super().train(sub_train=train_size))
+        pool = []
+        for i in range(voter):
+            thread = Thread(
+                target=self.gen_model,
+                args=(train_size, c1, c2)
+            )
+            thread.start()
+            pool.append(thread)
+        for thread in bar( pool ):
+            thread.join()
+            self.model_list.append(self.queue.get())
 
     def report(self):
         predict_res = []
