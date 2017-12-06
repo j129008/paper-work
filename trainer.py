b9ace88a6a609c2cfc86935e55fa818946599cff
j129008 <j129008@gmail.com>
2017-12-07 03:13:13 +0800

    add subplot

--- trainer.py
+++ trainer.py
@@ -4,27 +4,34 @@ from lib.learner import Learner
 from lib.feature import Feature
 from lib.ensumble_learner import Boosting
 from lib.ensumble_learner import Bagging
+from tqdm import tqdm as bar
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-man = Bagging('./data/data4.txt')
-man.load_feature(funcs=[Feature.context], params=[{'k':2, 'n_gram':2}])
 
 p_list = []
 r_list = []
-v_list = []
+f1_list = []
+x_list = []
 
-for voter in range(2, 10):
-    man.train(voter=voter)
+for x in bar( range(2, 5) ):
+    man = Boosting('./data/data4.txt')
+    man.load_feature(funcs=[Feature.context], params=[{'k':x, 'n_gram':2}])
+    man.train()
     p_list.append(man.get_score()['P'])
     r_list.append(man.get_score()['R'])
-    v_list.append(voter)
+    f1_list.append(man.get_score()['f1'])
+    x_list.append(x)
 
-plt.subplot(211)
-plt.plot(v_list, p_list)
+plt.subplot(311).title.set_text('precision')
+plt.plot(x_list, p_list)
 
-plt.subplot(212)
-plt.plot(v_list, r_list)
+plt.subplot(312).title.set_text('recall')
+plt.plot(x_list, r_list)
 
+plt.subplot(313).title.set_text('f1')
+plt.plot(x_list, f1_list)
+
+plt.tight_layout()
 plt.savefig('/mnt/d/trainer.png',dpi=300,format="png")
