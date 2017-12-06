a823a1ca4066c464b24cf73d0f6a2e7f06cf4c79
j129008 <j129008@gmail.com>
2017-12-07 00:03:04 +0800

    update trainer

--- trainer.py
+++ trainer.py
@@ -1,10 +1,10 @@
 from lib.crf import CRF
 from lib.data import Data
 from lib.learner import Learner
-from lib.feature import *
+from lib.feature import Feature
 from lib.ensumble_learner import Boosting
 
-boosting = Boosting('./data/data2.txt')
-boosting.feature_loader(funcs=[context], params=[[]])
+boosting = Boosting('./data/data4.txt')
+boosting.feature_loader(funcs=[Feature.context], params=[[]])
 boosting.train()
 boosting.report()
