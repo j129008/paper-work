3756874fc6f3df402043fcd7929f7fec6af24c61
j129008 <j129008@gmail.com>
2017-12-07 02:51:00 +0800

    add ploter

--- trainer.py
+++ trainer.py
@@ -3,8 +3,28 @@ from lib.data import Data
 from lib.learner import Learner
 from lib.feature import Feature
 from lib.ensumble_learner import Boosting
+from lib.ensumble_learner import Bagging
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
-man = Learner('./data/data2.txt')
+man = Bagging('./data/data4.txt')
 man.load_feature(funcs=[Feature.context], params=[{'k':2, 'n_gram':2}])
-man.train()
-man.report()
+
+p_list = []
+r_list = []
+v_list = []
+
+for voter in range(2, 10):
+    man.train(voter=voter)
+    p_list.append(man.get_score()['P'])
+    r_list.append(man.get_score()['R'])
+    v_list.append(voter)
+
+plt.subplot(211)
+plt.plot(v_list, p_list)
+
+plt.subplot(212)
+plt.plot(v_list, r_list)
+
+plt.savefig('/mnt/d/trainer.png',dpi=300,format="png")
