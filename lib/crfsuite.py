8d1cd05680236bb747ffa8d5709b284f1b65d70a
j129008 <j129008@gmail.com>
2017-12-08 07:53:40 +0800

    remove crfsuite.py bug ( final newline and miss param )

--- lib/crfsuite.py
+++ lib/crfsuite.py
@@ -4,7 +4,7 @@ class Crfsuite(Learner):
     def __init__(self, path):
         super().__init__(path)
     def crfSuite_transform(self):
-        def transform(self, X, Y,output_file=None):
+        def transform(X, Y,output_file=None):
             key_list = sorted( list( X[0].keys() ) )
             data_pool = []
             print('transform to crfSuite:')
@@ -15,6 +15,6 @@ class Crfsuite(Learner):
                     instance.append( str( X[i][key] ) )
                 data_pool.append( '\t'.join( instance ) )
             print('write to file: ', output_file)
-            open(output_file, 'w').write('\n'.join(data_pool))
+            open(output_file, 'w').write('\n'.join(data_pool)+'\n')
         transform(self.X_train, self.Y_train, output_file='./train.txt')
         transform(self.X_private, self.Y_private, output_file='./test.txt')
