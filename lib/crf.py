047f411e037656c189adfcde05eb843bd634b8f2
j129008 <j129008@gmail.com>
2017-12-12 17:05:46 +0800

    predict return prob

--- lib/crf.py
+++ lib/crf.py
@@ -7,3 +7,6 @@ class CRF(sklearn_crfsuite.CRF):
     def predict(self, x):
         res = super().predict([ [ele] for ele in x ])
         return [ ele[0] for ele in res ]
+    def predict_prob(self, x):
+        res = super().predict_marginals([ [ele] for ele in x ])
+        return [ ele[0]['E'] for ele in res ]
