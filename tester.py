a63546ea936bf31d796e1891eaed9390494472eb
j129008 <j129008@gmail.com>
2018-02-13 23:47:55 +0800

    new tester

--- tester.py
+++ tester.py
@@ -1,19 +1,34 @@
-from lib.data import Data
 from lib.feature import *
 from lib.crf import CRF
-from lib.metric import CompareFile
+from lib.metric import ErrorAnalyze
 from sklearn_crfsuite import metrics
+from pprint import pprint
 
-path = './data/data2.txt'
-train_data = Context(path)
-test_data = Context('./test.txt')
+train_file = './data/data4.txt'
+test_file = './data/test.txt'
 
+train_context = Context(train_file)
+test_context = Context(test_file)
+
+train_tdiff = Tdiff(train_file)
+test_tdiff = Tdiff(test_file)
+
+train_data = train_context + train_tdiff
+test_data = test_context + test_tdiff
 crf = CRF()
 crf.fit(train_data.X, train_data.Y)
 pred = crf.predict(test_data.X)
-
+ErrorAnalyze(file_name='pred/pred_tdiff.txt', pred=pred, feature_data=test_tdiff)
 print(metrics.flat_classification_report(
     test_data.Y, pred, labels=('I', 'E'), digits=4
 ))
 
-CompareFile(pred=pred, ans=test_data.Y, text=test_data.text)
+train_data = train_context
+test_data = test_context
+crf = CRF()
+crf.fit(train_data.X, train_data.Y)
+pred = crf.predict(test_data.X)
+ErrorAnalyze(file_name='pred/context_pred.txt', pred=pred, feature_data=test_context)
+print(metrics.flat_classification_report(
+    test_data.Y, pred, labels=('I', 'E'), digits=4
+))
