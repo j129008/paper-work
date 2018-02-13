536690840cdccc850fe534a668b86b471e86577b
j129008 <j129008@gmail.com>
2018-02-13 22:56:54 +0800

    new format for check error

--- lib/metric.py
+++ lib/metric.py
@@ -1,32 +1,13 @@
-#  import pdb
-class CompareFile:
-    def __init__(self, file_name='pred.txt', line_length=20, pred=None, ans=None, text=None):
-        #  pdb.set_trace()
+class ErrorAnalyze:
+    def __init__(self, file_name='pred.txt', pred=None, feature_data=None):
         f = open(file_name, 'w')
-        cutter = line_length
-        line_true = ''
-        line_pred = ''
+        truth = feature_data.Y
+        text = feature_data.text
         for i in range(len(text)):
-            word = text[i]
-            if i%cutter == 0:
-                f.write(line_true+'\n')
-                f.write(line_pred+'\n')
-                f.write('\n')
-                line_true = ''
-                line_pred = ''
-            line_pred+=word
-            line_true+=word
-            pred_w = pred[i]
-            real_w = ans[i]
-            if pred_w == real_w and real_w == 'E':
-                line_pred+='，'
-                line_true+='，'
-            if pred_w == 'E' and real_w == 'I':
-                line_pred+='，'
-                line_true+='　'
-            if pred_w == 'I' and real_w == 'E':
-                line_pred+='　'
-                line_true+='，'
-        f.write(line_true+'\n')
-        f.write(line_pred+'\n')
-        f.write('\n')
+            if pred[i] != truth[i]:
+                f.write('{}, T:{}, P:{}, {}\n'.format(text[i], truth[i], pred[i], feature_data.X[i]))
+            else:
+                if truth[i] == 'E':
+                    f.write('{}, {}\n\n'.format(text[i], feature_data.X[i]))
+                else:
+                    f.write('{}, {}\n'.format(text[i], feature_data.X[i]))
