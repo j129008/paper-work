d2b82db235f471c2dbd1ad5f97378a962e37ad2a
j129008 <j129008@gmail.com>
2018-02-14 02:50:30 +0800

    add feature compare metric

--- lib/metric.py
+++ lib/metric.py
@@ -10,3 +10,17 @@ class ErrorAnalyze:
                 f.write('{}, {}\n'.format(text[i], feature_data.X[i]))
             if truth[i] == 'E':
                 f.write('\n')
+
+class ErrorCompare:
+    def __init__(self, file_name='compare.txt', pred_list=None, feature_data=None):
+        f = open(file_name, 'w')
+        text = feature_data.text
+        truth = feature_data.Y
+        for i in range(len(text)):
+            pred_i = [truth[i]] + [ pred[i] for pred in pred_list]
+            if len(set(pred_i)) != 1:
+                f.write('{}, {}, {}\n'.format(text[i], pred_i[1:], feature_data.X[i]))
+            else:
+                f.write('{}\n'.format(text[i]) )
+            if truth[i] == 'E':
+                f.write('\n')
--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from lib.feature import *
 from lib.crf import CRF
-from lib.metric import ErrorAnalyze
+from lib.metric import ErrorAnalyze, ErrorCompare
 from sklearn_crfsuite import metrics
 from pprint import pprint
 
@@ -17,18 +17,20 @@ train_data = train_context + train_tdiff
 test_data = test_context + test_tdiff
 crf = CRF()
 crf.fit(train_data.X, train_data.Y)
-pred = crf.predict(test_data.X)
-ErrorAnalyze(file_name='pred/pred_tdiff.txt', pred=pred, feature_data=test_tdiff)
+pred_tdiff = crf.predict(test_data.X)
+ErrorAnalyze(file_name='pred/tdiff.txt', pred=pred_tdiff, feature_data=test_tdiff)
 print(metrics.flat_classification_report(
-    test_data.Y, pred, labels=('I', 'E'), digits=4
+    test_data.Y, pred_tdiff, labels=('I', 'E'), digits=4
 ))
 
 train_data = train_context
 test_data = test_context
 crf = CRF()
 crf.fit(train_data.X, train_data.Y)
-pred = crf.predict(test_data.X)
-ErrorAnalyze(file_name='pred/context_pred.txt', pred=pred, feature_data=test_context)
+pred_context = crf.predict(test_data.X)
+ErrorAnalyze(file_name='pred/context.txt', pred=pred_context, feature_data=test_context)
 print(metrics.flat_classification_report(
-    test_data.Y, pred, labels=('I', 'E'), digits=4
+    test_data.Y, pred_context, labels=('I', 'E'), digits=4
 ))
+
+ErrorCompare(file_name='./pred/compare.txt', pred_list=[pred_context, pred_tdiff], feature_data=test_tdiff)
