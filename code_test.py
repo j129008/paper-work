d9eae2a23dd343906220762562ef4483f081039a
j129008 <j129008@gmail.com>
2018-02-25 23:47:47 +0800

    finish unitest

--- code_test.py
+++ code_test.py
@@ -12,109 +12,93 @@ from keras.layers import LSTM, TimeDistributed, SimpleRNN, Embedding, RNN, GRU, 
 import unittest
 
 path = './data/data4.txt'
-context = Context(path)
 
-class CodeTest(unittest.TestCase):
-    def test_mi(self):
-        mi = MutualInfo(path)
-
-    def test_tdiff(self):
-        tdiff = Tdiff(path)
-
-    def test_rhyme(self):
-        rhyme = Rhyme(path, './data/rhyme.txt', './pickles/rhyme_list.pkl', ['反切', '聲母', '韻目', '調', '等', '呼', '韻母'])
-
-    def test_label(self):
-        office_data = Label(path, 'office', './ref/known/office2.txt')
-
-    def test_shrink(self):
+class CrfTest(unittest.TestCase):
+    def setUp(self):
         context = Context(path)
-        context.shrink()
-        learner = Learner(context)
-        learner.train()
-        print('shrink')
-        learner.report()
-
-    def test_data_adder(self):
         mi = MutualInfo(path)
-        data = mi+context
-        learner = Learner(data)
+        tdiff = Tdiff(path)
+        rhyme = Rhyme(path, './data/rhyme.txt', './pickles/rhyme_list.pkl', ['反切', '聲母', '韻目', '調', '等', '呼', '韻母'])
+        office_data = Label(path, 'office', './ref/known/office2.txt')
+        self.data = context + mi + tdiff + rhyme + office_data
+        self.data.shrink()
+        self.data.segment(length=10)
+
+    def test_boost(self):
+        print('boost')
+        learner = Boosting(self.data)
         learner.train()
-        print('mi+context')
+        print(learner.alpha_list)
         learner.report()
 
-    def test_vec(self):
-        data = UniVec(path)
-        x_train, x_test, y_train, y_test = train_test_split(
-            data.X, data.Y, test_size=0.6, random_state=1, shuffle=False
-        )
-        clf = RandomForestClassifier()
-        clf.fit(x_train, y_train)
+    def test_bagging(self):
+        print('bagging')
+        learner = Bagging(self.data)
+        learner.train()
+        learner.report()
 
-        y_pred = clf.predict( x_test )
-        y_pred = data.y2lab(y_pred)
-        y_test = data.y2lab(y_test)
-        print('random forest:')
-        print(metrics.flat_classification_report(
-            y_test, y_pred, labels=('I', 'E'), digits=4
-        ))
+    def test_learner(self):
+        print('crf')
+        learner = Learner(self.data)
+        learner.train()
+        learner.report()
 
-    def test_keras(self):
-        voc_size = len(set(open(path, 'r').read()))
-        k = 1
-        data = VecContext(path, k=k)
-        x_train, x_test, y_train, y_test = train_test_split(
-            data.X, data.Y, test_size=0.6, random_state=1, shuffle=False
+    def test_CV(self):
+        print('crf-CV-tune')
+        learner = Learner(self.data)
+        learner.train_CV()
+        learner.report()
+
+    def test_analyzer(self):
+        learner = Learner(self.data)
+        learner.train()
+        pred = learner.predict(learner.X_private)
+        ErrorAnalyze(pred=pred, feature_data=learner)
+        ErrorCompare(pred_list=[pred, pred], feature_data=learner)
+
+class KerasTest(unittest.TestCase):
+    def setUp(self):
+        self.k = 1
+        self.data = VecContext(path, k=self.k)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.data.X, self.data.Y, test_size=0.6, random_state=1, shuffle=False
         )
+
+    def test_biLSTM(self):
         model = Sequential()
         for _ in range(4):
-            model.add(Bidirectional(LSTM(50, return_sequences=True, go_backwards=True), input_shape=(k*2+1, 100)))
+            model.add(Bidirectional(LSTM(50, return_sequences=True, go_backwards=True), input_shape=(self.k*2+1, 100)))
         model.add(Bidirectional(LSTM(50)))
         model.add(Dense(1, activation='sigmoid'))
         model.compile(loss='binary_crossentropy',
                       optimizer='adam',
                       metrics=['accuracy'])
-        model.fit(x_train, y_train, batch_size=100, epochs=1)
-        pred = model.predict(x_test)
-        y_pred = data.y2lab(pred)
-        y_test = data.y2lab(y_test)
+        model.fit(self.x_train, self.y_train, batch_size=100, epochs=1)
+        pred = model.predict(self.x_test)
+        y_pred = self.data.y2lab(pred)
+        y_test = self.data.y2lab(self.y_test)
         print('keras')
         print(metrics.flat_classification_report(
             y_test, y_pred, labels=('I', 'E'), digits=4
         ))
 
-    def test_boost(self):
-        context = Context(path)
-        context.segment(length=10)
-        learner = Boosting(context)
-        learner.train()
-        print(learner.alpha_list)
-        print('boost')
-        learner.report()
+class VecModelTest(unittest.TestCase):
+    def setUp(self):
+        self.data = UniVec(path)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.data.X, self.data.Y, test_size=0.6, random_state=1, shuffle=False
+        )
 
-    def test_bagging(self):
-        learner = Bagging(context)
-        learner.train()
-        print('bagging')
-        learner.report()
+    def test_RandomForest(self):
+        clf = RandomForestClassifier()
+        clf.fit(self.x_train, self.y_train)
 
-    def test_learner(self):
-        learner = Learner(context)
-        learner.train()
-        print('crf')
-        learner.report()
-
-    def test_CV(self):
-        learner = Learner(context)
-        learner.train_CV()
-        print('crf-CV-tune')
-        learner.report()
-
-    def test_analyzer(self):
-        learner = Learner(context)
-        learner.train()
-        pred = learner.predict(learner.X_private)
-        ErrorAnalyze(pred=pred, feature_data=learner)
-        ErrorCompare(pred_list=[pred, pred], feature_data=learner)
+        y_pred = clf.predict(self.x_test)
+        y_pred = self.data.y2lab(y_pred)
+        y_test = self.data.y2lab(self.y_test)
+        print('random forest:')
+        print(metrics.flat_classification_report(
+            y_test, y_pred, labels=('I', 'E'), digits=4
+        ))
 
 unittest.main()
