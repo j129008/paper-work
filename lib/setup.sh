768a8ac252db2ed86081464e99eb766cf1a03c3c
j129008 <j129008@gmail.com>
2018-03-17 03:51:45 +0800

    add pandas

--- lib/setup.sh
+++ lib/setup.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get install python3-dev build-essential unrar unzip
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
-sudo pip3 install numpy scipy sklearn sklearn-crfsuite regex
+sudo pip3 install numpy scipy sklearn sklearn-crfsuite regex pandas
