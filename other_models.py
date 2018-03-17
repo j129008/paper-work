c0241a2bbac1b27c1b33d23c63ead9b57c1394bc
j129008 <j129008@gmail.com>
2018-03-18 04:35:32 +0800

    other models use Kfold

--- other_models.py
+++ other_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 from lib.learner import Learner
 from lib.feature import *
 from tqdm import tqdm as bar
@@ -9,16 +10,15 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
 from sklearn_crfsuite import metrics
 import csv
 
 path = './data/data_proc.txt'
 result_table = csv.writer( open('./other_model.csv', 'w') )
 vec = UniVec(path)
-x_train, x_test, y_train, y_test = train_test_split(
-    vec.X, vec.Y, test_size=0.4
-)
-y_test = vec.y2lab(y_test)
+X = np.array(vec.X)
+Y = np.array(vec.Y)
 
 clf_list = [
     RandomForestClassifier(n_jobs=8),
@@ -28,15 +28,24 @@ clf_list = [
     GaussianNB(),
     DecisionTreeClassifier()
 ]
+
+kf = KFold(n_splits=3)
 for clf in bar(clf_list):
-    clf.fit(x_train, y_train)
-
-    pred_vec = clf.predict(x_test)
-    pred_lab = vec.y2lab(pred_vec)
-
-    label = 'E'
-    P = metrics.flat_precision_score(y_test, pred_lab, pos_label=label)
-    R = metrics.flat_recall_score(y_test, pred_lab, pos_label=label)
-    f1 = metrics.flat_f1_score(y_test, pred_lab, pos_label=label)
-    print(P, R, f1)
-    result_table.writerow([P, R, f1])
+    P_list = []
+    R_list = []
+    f1_list = []
+    for train, test in kf.split(X):
+        clf.fit(X[train], Y[train])
+        pred_vec = clf.predict(X[test])
+        pred_lab = vec.y2lab(pred_vec)
+        y_test = vec.y2lab(Y[test])
+        label = 'E'
+        P = metrics.flat_precision_score(y_test, pred_lab, pos_label=label)
+        R = metrics.flat_recall_score(y_test, pred_lab, pos_label=label)
+        f1 = metrics.flat_f1_score(y_test, pred_lab, pos_label=label)
+        P_list.append(P)
+        R_list.append(R)
+        f1_list.append(f1)
+        print(P, R, f1)
+    avg = lambda l:sum(l)/len(l)
+    result_table.writerow([avg(P_list), avg(R_list), avg(f1_list)])
