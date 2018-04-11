10195e592a7bf836a7db088b7dd61ee73c8d97e1
j129008 <j129008@gmail.com>
2018-04-11 23:07:00 +0800

    add vec weight param

--- lib/feature.py
+++ lib/feature.py
@@ -147,15 +147,16 @@ class BigramVecContext(VecContext):
         self.X = X
 
 class UniVec(VecContext):
-    def __init__(self, path, k=1, vec_size=50, mode='chain'):
+    def __init__(self, path, k=1, vec_size=50, mode='chain', weight=None):
         super().__init__(path, k=k, vec_size=vec_size)
         if mode == 'chain':
             self.X = [ list(chain(*vec_list)) for vec_list in self.X ]
         elif mode == 'average':
             self.X = [ [ sum(u)/len(u) for u in zip(*vec_list) ] for vec_list in self.X  ]
         elif mode == 'weight':
-            w = list(range(1, k))
-            weight = w + [k] + list(reversed(w))
+            if weight == None:
+                w = list(range(1, k+1))
+                weight = w + [k] + list(reversed(w))
             vec_mul = lambda v1, v2 : [ ele[0]*ele[1] for ele in zip(v1, v2) ]
             self.X = [ [ sum(vec_mul(u, weight))/len(u) for u in zip(*vec_list) ] for vec_list in self.X  ]
 
