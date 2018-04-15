3acd498bd7fdcbd595bbcb1962c48478be99883a
j129008 <j129008@gmail.com>
2018-04-15 17:51:11 +0800

    add mm file

---
+++ seg_s2s_deep_mm.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+from lib.feature import *
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Input, concatenate, RepeatVector
+from sklearn_crfsuite import metrics
+import keras
+from keras.callbacks import EarlyStopping, TensorBoard, CSVLogger
+from keras.optimizers import Adam
+from keras.layers import CuDNNLSTM, TimeDistributed, SimpleRNN, Embedding, RNN, GRU, Bidirectional, CuDNNLSTM
+from sklearn.model_selection import train_test_split
+from random import randint
+from pprint import pprint
+
+def chunks(l, n):
+    for i in range(0, len(l)-len(l)%n, n):
+        yield np.array(l[i:i + n])
+
+def batch_gen(x, y, seq_len, batch_size=100):
+    while True:
+        batch_x = []
+        batch_y = []
+        for i in range(len(y)-seq_len):
+            seq_x = x[i:i+seq_len]
+            seq_y = y[i:i+seq_len]
+            batch_x.append(seq_x)
+            batch_y.append(seq_y)
+            if len(batch_x) == batch_size:
+                _batch_x = np.array(batch_x)
+                _batch_y = np.array(batch_y)
+                batch_x = []
+                batch_y = []
+                yield (_batch_x, _batch_y)
+
+def predict_gen(x, seq_len, batch_size=100):
+    while True:
+        batch_x = []
+        for i in range(len(x)-seq_len):
+            seq_x = x[i:i+seq_len]
+            batch_x.append(seq_x)
+            if len(batch_x) == batch_size:
+                _batch_x = np.array(batch_x)
+                batch_x = []
+                yield _batch_x
+
+path = './data/data_proc.txt'
+data = UniVec(path, k=0, vec_size=50)
+data.Y = [ [ele] for ele in data.Y ]
+
+for k in range(9, 15):
+    seg_size = 2*k + 1
+    print('k:', k, file=open('lstm_seg.txt', 'a'))
+    x_train, x_test, y_train, y_test = train_test_split(
+        data.X, data.Y, test_size=0.3, shuffle=False
+    )
+
+    trans = lambda data : np.array(list(chunks(data, seg_size)))
+    split_p = len(x_train)//10
+    x_vaild = x_train[-split_p:]
+    y_vaild = y_train[-split_p:]
+    x_train = x_train[:-split_p]
+    y_train = y_train[:-split_p]
+
+    print('start training')
+    inputs = Input(shape=(seg_size, 50))
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(inputs)
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(x)
+    x = Dropout(0.2)(x)
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(x)
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(x)
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(x)
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(x)
+    main_output = TimeDistributed(Dense(1, activation='sigmoid'))(x)
+
+    model = Model(inputs=[inputs], outputs=main_output)
+    keras.utils.plot_model(model, to_file='model.png')
+    model.summary()
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, mode='min')
+    model.fit_generator(batch_gen(x_train, y_train, seq_len=seg_size, batch_size=100), steps_per_epoch=len(y_train)//5, epochs=100, validation_data=batch_gen(x_vaild, y_vaild, seq_len=seg_size), validation_steps=len(y_vaild)//5, callbacks=[early_stop])
+    pred = model.predict_generator(predict_gen(x_test, seq_len=seg_size), steps=(len(y_test)//100)+1, verbose=1)
+    pred = list([ seq[0] for seq in pred ]) + list(pred[-1][1:])
+    union = lambda data: [ ins for chap in data for ins in chap ]
+    y_pred = data.y2lab(union(pred))
+    y_test = data.y2lab(union(y_test))
+    print(metrics.flat_classification_report(
+        y_test, y_pred[:len(y_test)], labels=('I', 'E'), digits=4
+    ), file=open('lstm_seg.txt', 'a'))
