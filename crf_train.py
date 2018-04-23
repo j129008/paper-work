3a3648685d1004a99c180198fb228ae0d2a0fae7
j129008 <j129008@gmail.com>
2018-04-23 20:31:46 +0800

    crf/lstm train file

---
+++ crf_train.py
@@ -0,0 +1,18 @@
+from lib.data import Data
+from lib.learner import Learner
+from lib.feature import *
+from lib.ensumble_learner import Bagging, Boosting
+from lib.metric import *
+from sklearn.model_selection import train_test_split
+from sklearn_crfsuite import metrics
+
+train_path = './data/train.txt'
+crf_k      = 5
+
+# CRF
+crf_train  = Context(train_path, k=crf_k)
+man = Learner(crf_train)
+man.X_train = man.X
+man.Y_train = man.Y
+man.train()
+man.save('./pickles/crf_model.pkl')
---
+++ deep_crf.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+import pickle
+from lib.feature import *
+from keras.models import load_model
+from sklearn_crfsuite import metrics
+import numpy as np
+
+test_path = './data/test.txt'
+crf_k     = 5
+deep_k    = 12
+
+# CRF
+crf_test  = Context(test_path, k=crf_k)
+crf = pickle.load(open('./pickles/crf_model.pkl', 'rb'))
+crf_pred = crf.predict_prob(crf_test.X)
+union = lambda x: [ ins for chap in x for ins in chap ]
+crf_pred = [ ele['E'] for ele in union(crf_pred) ]
+ans = union(crf_test.Y)
+
+# LSTM
+deep_test = VecContext(test_path, k=deep_k, vec_size=50, w2v_text='./data/w2v.txt')
+model = load_model('./pickles/keras.h5')
+deep_pred = model.predict([np.array(deep_test.X)])
+deep_pred = union(deep_pred)
+avg = lambda x, y: [ (x[i]+y[i])/2 for i in range(len(x)) ]
+crf_deep = avg(crf_pred, deep_pred)
+label_crf_deep = deep_test.y2lab(crf_deep)
+label_deep = deep_test.y2lab(deep_pred)
+label_crf = deep_test.y2lab(crf_pred)
+
+print('average:')
+print(metrics.flat_classification_report(
+    ans, label_crf_deep, labels=('I', 'E'), digits=4
+))
+print('LSTM:')
+print(metrics.flat_classification_report(
+    ans, label_deep, labels=('I', 'E'), digits=4
+))
+print('CRF:')
+print(metrics.flat_classification_report(
+    ans, label_crf, labels=('I', 'E'), digits=4
+))
