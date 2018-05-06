530db50096e8d6d6a67d700f50292cd86e9bfeb5
j129008 <j129008@gmail.com>
2018-05-06 17:19:17 +0800

    shuffle chap

---
+++ data_shuffle.py
@@ -0,0 +1,13 @@
+from random import shuffle
+
+text_list = [ line for line in open('./data/data_proc.txt', 'r') ]
+shuffle(text_list)
+
+spliter = int(len(text_list) * 0.7)
+fw = open('./data/train.txt', 'w')
+fw.writelines(text_list[:spliter])
+fw.close()
+
+fw = open('./data/test.txt', 'w')
+fw.writelines(text_list[spliter:])
+fw.close()
