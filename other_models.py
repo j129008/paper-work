74e27e4a0047ed27d0b41eab67865dcd4d3d7c5f
j129008 <j129008@gmail.com>
2018-05-11 21:14:03 +0800

    other models add lgb

--- other_models.py
+++ other_models.py
@@ -12,9 +12,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
 from sklearn_crfsuite import metrics
+import lightgbm as lgb
 import csv
 
-path = './data/data_proc.txt'
+path = './data/data_lite.txt'
 result_table = csv.writer( open('./csv/other_model.csv', 'w') )
 vec = UniVec(path, vec_size=50, k=1, mode='chain')
 X = np.array(vec.X)
@@ -25,23 +26,55 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 clf_list = [
-    RandomForestClassifier(n_jobs=8),
-    KNeighborsClassifier(n_neighbors=8, n_jobs=8),
-    AdaBoostClassifier(),
-    MLPClassifier(hidden_layer_sizes=(20,10,5), alpha=1e-10),
-    GaussianNB(),
-    DecisionTreeClassifier(),
-    svm.SVC(C=1000, max_iter=5000)
+    ('random forest', RandomForestClassifier(n_jobs=8)),
+    ('adaboost', AdaBoostClassifier()),
+    ('MLP', MLPClassifier(hidden_layer_sizes=(20,10,5), alpha=1e-10)),
+    ('GaussianNB', GaussianNB()),
+    ('svm', svm.SVC(C=1000, max_iter=5000)),
+    ('decision tree', DecisionTreeClassifier()),
 ]
 
-for clf in bar(clf_list):
+def report(pred, truth, csv_table, clf_name):
+    label = 'E'
+    pred_lab = VecContext.y2lab(pred)
+    truth_lab = VecContext.y2lab(truth)
+    P = metrics.flat_precision_score(truth_lab, pred_lab, pos_label=label)
+    R = metrics.flat_recall_score(truth_lab, pred_lab, pos_label=label)
+    f1 = metrics.flat_f1_score(truth_lab, pred_lab, pos_label=label)
+    print(clf_name)
+    print(metrics.flat_classification_report(
+        truth_lab, pred_lab, labels=('I', 'E'), digits=4
+    ))
+    csv_table.writerow([clf_name, P, R, f1])
+
+for clf_name, clf in bar(clf_list):
     clf.fit(x_train, y_train)
     pred_vec = clf.predict(x_test)
-    pred_lab = vec.y2lab(pred_vec)
-    _y_test = vec.y2lab(y_test)
-    label = 'E'
-    P = metrics.flat_precision_score(_y_test, pred_lab, pos_label=label)
-    R = metrics.flat_recall_score(_y_test, pred_lab, pos_label=label)
-    f1 = metrics.flat_f1_score(_y_test, pred_lab, pos_label=label)
-    print(P, R, f1)
-    result_table.writerow([P, R, f1])
+    report(pred_vec, y_test, result_table, clf_name)
+
+
+# lgb
+x_train, x_valid, y_train, y_valid = train_test_split(
+    x_train, y_train, test_size=0.1, shuffle=True
+)
+
+train_data = lgb.Dataset(x_train, y_train)
+valid_data = lgb.Dataset(x_valid, y_valid, reference=train_data)
+
+params = {
+        'task'             : 'train',
+        'boosting_type'    : 'gbdt',
+        'objective'        : 'binary',
+        'metric'           : {'l2', 'auc'},
+        'num_leaves'       : 2047,
+        'learning_rate'    : 0.05,
+        'feature_fraction' : 0.9,
+        'bagging_fraction' : 0.8,
+        'bagging_freq'     : 5,
+        'verbose'          : 0
+}
+num_round = 1000
+bst = lgb.train(params, train_data, num_round, valid_sets=[valid_data], early_stopping_rounds=10)
+
+pred = bst.predict(x_test)
+report(pred, y_test, result_table, 'lgb')
