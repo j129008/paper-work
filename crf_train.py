2bb306166ee265a70772df8a80af8c4d7541db18
j129008 <j129008@gmail.com>
2018-05-14 02:01:02 +0800

    best crf_train

--- crf_train.py
+++ crf_train.py
@@ -6,13 +6,15 @@ from lib.metric import *
 from sklearn.model_selection import train_test_split
 from sklearn_crfsuite import metrics
 
-train_path = './data/train-valid.txt'
+train_path = './data/train.txt'
 crf_k      = 5
 
 # CRF
 crf_train  = Context(train_path, k=crf_k)
-man = Learner(crf_train)
+tdiff = Tdiff(train_path)
+pmi = MutualInfo(train_path)
+man = Learner(crf_train + tdiff + pmi)
 man.X_train = man.X
 man.Y_train = man.Y
-man.train()
-man.save('./pickles/crf_model-valid.pkl')
+man.train_CV(n_iter=4, cv=6)
+man.save('./pickles/crf_model-best.pkl')
