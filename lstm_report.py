ad5f678a71371004e8ea668b09450c35d3fcb69c
j129008 <j129008@gmail.com>
2018-05-20 23:26:15 +0800

    add noise test

--- lstm_report.py
+++ lstm_report.py
@@ -76,6 +76,40 @@ for aux_name, aux_train, aux_test in [ ('tdiff', tdiff_train, tdiff_test), ('pmi
     score = report(pred, y_test)
     result_table.writerow([aux_name, score['P'], score['R'], score['f1']])
 
+# noise
+x_train, x_test, y_train, y_test = context_data(path, k=k_baseline)
+aux_train, aux_test = aux_adv_data(path)
+tdiff_train, mi_train = list(zip(*aux_train))
+tdiff_test, mi_test, office_test, address_test, nianhao_test = list(zip(*aux_test))
+
+for aux_name, aux_train, aux_test in [ ('tdiff-noise', tdiff_train, tdiff_test), ('pmi-noise', mi_train, mi_test) ]:
+    print(aux_name)
+    aux_train = np.array(aux_train).reshape(-1, 1)
+    aux_test = np.array(aux_test).reshape(-1, 1)
+    inputs = Input(shape=(len(x_test[0]), len(x_test[0][0])))
+    x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(inputs)
+    for _ in range(3):
+        x = Bidirectional(CuDNNLSTM(50, return_sequences=True))(x)
+    lstm_output= Bidirectional(CuDNNLSTM(50))(x)
+
+    aux_input = Input(shape=(len(aux_train[0]),))
+    x = concatenate([lstm_output, aux_input])
+    x = Dense(50, activation='relu')(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dense(50, activation='relu')(x)
+    main_output = Dense(1, activation='sigmoid')(x)
+
+    model = Model(inputs=[inputs, aux_input], outputs=main_output)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    model.fit([x_train, aux_train], y_train, batch_size=100, callbacks=[early_stop], validation_split=0.1, epochs=100)
+    pred = model.predict([x_test, aux_test])
+    score = report(pred, y_test)
+    result_table.writerow([aux_name, score['P'], score['R'], score['f1']])
+
 # seq2seq
 print('s2s')
 x_train, x_test, y_train, y_test = context_data(path, k=k_baseline, seq=True)
