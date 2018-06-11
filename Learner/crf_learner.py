7498b963e9fb3f8c131262c995866a1b1e6e240a
j129008 <j129008@gmail.com>
2018-06-12 02:24:06 +0800

    args learner

--- Learner/crf_learner.py
+++ Learner/crf_learner.py
@@ -1,12 +1,20 @@
 from lib.learner import Learner
-from lib.feature import Context, MutualInfo
+from lib.arg import crf_arg, crf_data
 
-path = '../data/data_lite.txt'
-data = Context(path) + MutualInfo(path)
+parser = crf_arg()
+parser.add_argument('-iter', dest='iter', type=int, default=None)
+parser.add_argument('-cv', dest='cv', type=int, default=None)
+parser.add_argument('-c1', dest='c1', type=float, default=0.0)
+parser.add_argument('-c2', dest='c2', type=float, default=1.0)
+parser.add_argument('-size', dest='size', type=float, default=1.0)
+args = parser.parse_args()
+data = crf_data(args)
+
 man = Learner(data)
 
-man.train(c1=0, c2=1, sub_train=0.2)
-man.report()
-
-man.train_CV(cv=3, n_iter=1)
-man.report()
+if args.cv != None and args.iter != None:
+    man.train_CV(cv=args.cv, n_iter=args.iter)
+    man.report()
+else:
+    man.train(c1=args.c1, c2=args.c2, sub_train=args.size)
+    man.report()
