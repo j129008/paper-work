79d499269e8e1e367d89f1cc47a632d736bb8a3d
j129008 <j129008@gmail.com>
2018-06-12 03:35:51 +0800

    update bagging

--- Learner/crf_bagging.py
+++ Learner/crf_bagging.py
@@ -2,10 +2,17 @@ from lib.ensumble_learner import Bagging
 from lib.arg import crf_arg, crf_data
 
 parser = crf_arg()
+parser.add_argument('-trainsplit', dest='trainsplit', type=float, default=0.7)
+parser.add_argument('-seg', dest='seg', type=int, default=1)
+parser.add_argument('-voter', dest='voter', type=int, default=8)
+parser.add_argument('-votersize', dest='votersize', type=float, default=1.4)
 args = parser.parse_args()
 data = crf_data(args)
-data.segment(length=1)
+data.segment(length=args.seg)
 
 man = Bagging(data)
-man.train()
-man.report()
+man.split_data(train_size=args.trainsplit)
+man.train(voter=args.voter, train_size=args.votersize)
+
+if args.trainsplit < 1:
+    man.report()
