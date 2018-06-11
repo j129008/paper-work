6a7d969f92aa6fc5ac82ac9f6f2ad19d4dad7766
j129008 <j129008@gmail.com>
2018-06-11 23:36:37 +0800

    lib change to ln

--- Text_Classify/lib/setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get install python3-dev build-essential unrar unzip
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-sudo pip3 install numpy scipy sklearn sklearn-crfsuite regex pandas
