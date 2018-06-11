cf25825d8fd9b8e0c91dc7eacaf56813252b23f3
j129008 <j129008@gmail.com>
2018-06-12 02:42:48 +0800

    update args in learner

--- Learner/crf_learner.py
+++ Learner/crf_learner.py
@@ -6,15 +6,22 @@ parser.add_argument('-iter', dest='iter', type=int, default=None)
 parser.add_argument('-cv', dest='cv', type=int, default=None)
 parser.add_argument('-c1', dest='c1', type=float, default=0.0)
 parser.add_argument('-c2', dest='c2', type=float, default=1.0)
-parser.add_argument('-size', dest='size', type=float, default=1.0)
+parser.add_argument('-subtrain', dest='subtrain', type=float, default=1.0)
+parser.add_argument('-trainsplit', dest='trainsplit', type=float, default=0.7)
+parser.add_argument('-savemodel', dest='save', default=None)
 args = parser.parse_args()
 data = crf_data(args)
 
 man = Learner(data)
+man.split_data(train_size=args.trainsplit)
 
 if args.cv != None and args.iter != None:
     man.train_CV(cv=args.cv, n_iter=args.iter)
-    man.report()
 else:
-    man.train(c1=args.c1, c2=args.c2, sub_train=args.size)
+    man.train(c1=args.c1, c2=args.c2, sub_train=args.subtrain)
+
+if args.trainsplit < 1.0:
     man.report()
+
+if args.save != None:
+    man.save(args.save)
