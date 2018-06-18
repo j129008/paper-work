a6e5edbde8f84956e601cdf8a80aa20af371bc9f
j129008 <j129008@gmail.com>
2018-06-18 22:23:33 +0800

    add linker

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function linker {
+   cd $1
+   pwd
+   ln -sf ../data
+   ln -sf ../lib
+   ln -sf ../ref
+   ln -sf ../pickles
+   cd ..
+}
+
+linker CRF_Learner
+linker LSTM_Learner
+linker CRF_LSTM_Ensemble
+linker Text2Feature
+linker Text_Classify
+linker Text_Preproc
+linker Other_Model
