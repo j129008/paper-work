097bcf92a6856058df154ca80fb32f58c58073dd
j129008 <j129008@gmail.com>
2018-06-19 13:39:49 +0800

    add lstm_stack

--- LSTM_Learner/lstm_stack.sh
+++ LSTM_Learner/lstm_stack.sh
@@ -3,5 +3,5 @@ END=10
 for i in $(seq 1 $END);
 do
    echo 'lstm stack :' $i
-   time python3 ./lstm_basic.py -i ./data/data_lite.txt -k 10 -ts 0.7 -stack $i
+   time python3 ./lstm_basic.py -i $1 -k 10 -ts 0.7 -stack $i
 done
