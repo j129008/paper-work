df9ce34a5728677555336a9f16f7fd0563e3bad2
j129008 <j129008@gmail.com>
2018-06-19 19:16:35 +0800

    auto rm w2v.pkl

--- preproc.sh
+++ preproc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RAW=./data/epitaph_RAW.txt
+RAW=./data/buddhist_info_RAW.txt
 DATA=./data/tang_epitaph.txt
 TRAIN=./data/train.txt
 TEST=./data/test.txt
@@ -10,3 +10,6 @@ echo 'text preproc'
 ./text_preproc.sh $RAW $DATA
 ./text_split.sh $DATA $TRAIN $TEST $SPLIT
 cd ..
+
+echo 'remove word2vec.pkl'
+rm -f ./pickles/word2vec.pkl
