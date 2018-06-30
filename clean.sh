80f3408b39ee2d3be4fba4ebf888d6aa56d57799
j129008 <j129008@gmail.com>
2018-06-30 15:36:34 +0800

    add clean sh

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -f ./data/*.pred
+rm -f ./data/*.re
+rm -f ./data/*.train
+rm -f ./data/*.test
+rm -f ./data/w2v.txt
+rm -f ./pickles/crf.pkl
+rm -f ./pickles/lstm.h5
+rm -f ./pickles/word2vec.pkl
